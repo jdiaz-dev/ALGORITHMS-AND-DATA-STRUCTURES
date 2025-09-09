10693a9a2d3bd5c6a674fb5112d50e4762d8519c
polsito <lordgelsin26@gmail.com>
2025-09-09 17:15:17 -0500

    fix

---
+++ CAPITULO 1/Big O Notation/notes.js
@@ -0,0 +1,15 @@
+/* 
+    what means big o notation ?
+        Big-O notation is a way to describe how the running time or space usage of an algorithm grows as the input size grows.
+
+*/
+
+/* 
+    What "input size" (n) means in Big-O notation?
+        - The input size (n) is a measure of how much data your algorithm has to work with.
+        It depends on the problem:
+
+        For arrays/lists: the number of elements.
+        Example: if you have [1, 2, 3, 4, 5], then n = 5.
+
+*/
